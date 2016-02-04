81cf61934914e2f4afbaf5165314bf04d7127f59
webpanos <info@web10.gr>
2016-02-04 19:01:44 +0200

    absolute links

--- services/config.php
+++ services/config.php
@@ -1,5 +1,6 @@
 <?php
 	
+//	$dbhost = '79.124.76.85:3306'; 
 	$dbhost = 'localhost';
 	$dbuser = 'root';
 	$dbpass = '';
--- services/configRem.php
+++ services/configRem.php
@@ -1,7 +1,8 @@
 <?php
 	
 // Define connection parameters ++++++ CPanel
-	$dbhost = '79.124.76.85:3306'; 
+//	$dbhost = '79.124.76.85:3306'; 
+	$dbhost = 'localhost';
 	$dbuser = 'astypain_panos';
 	$dbpass = 'sporDB2014$#';
 	$dbname = 'astypain_astypalaia2';
