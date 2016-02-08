388f44e6d11e489d8b4705105d087be5402e860c
webpanos <info@web10.gr>
2016-02-08 12:01:33 +0200

    callback

--- services/getAccommodationNoDB.php
+++ services/getAccommodationNoDB.php
@@ -7,7 +7,7 @@
 	
 try {
 	
-	$numOfProperties = array(28,1,4,1,0,"",16,7,5,6);
+	$numOfProperties = array(28,99,4,1,0,"",16,7,5,6);
 
     echo ($callback ? $callback . '(' : '') . '{"numOfProperties":'. json_encode($numOfProperties) .'}' . ($callback ? ')' : '');
 	
